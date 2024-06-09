d7575be3165833fb214c1c251c51c03e5866a54f
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-06-09 23:42:02 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -14,7 +14,7 @@ Mark = 'X'
 
 def DrawBoard():
     print(" %c | %c | %c " % (board[1],board[2],board[3]))
-    
+   
     print("___|___|___")
     print(" %c | %c | %c " % (board[4],board[5],board[6]))
     print("___|___|___")
